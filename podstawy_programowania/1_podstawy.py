9a1d396c1cd5899d8c9b469ac597c7232b06be55
wegrzynl <eberhard3079@gmail.com>
2020-03-29 13:26:50 +0200

    string

--- podstawy_programowania/1_podstawy.py
+++ podstawy_programowania/1_podstawy.py
@@ -4,3 +4,11 @@ print(8)
 #potęgowanie
 pow = 2 ** 10
 print(pow)
+
+text = "CDV"
+print(text * 2)
+
+#pobieranie danych z klawiatury
+# + kokatenacja (połączenie dwóch stringów)
+name = input()
+print("Twoje imie: " + name)
